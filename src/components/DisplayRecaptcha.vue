9f062a75dd2f692713fa48cbb841eaaf19910d02
ffullex <mr.tomanovskiy@mail.ru>
2021-11-23 09:22:37 +0700

    Initial commit

---
+++ src/components/DisplayRecaptcha.vue
@@ -0,0 +1,25 @@
+<template>
+  <vue-recaptcha
+      sitekey="6Lc0mkQdAAAAAAy2wzBniXg8zGFNDsu_Gv_mKsxn"
+      @verify="successVerify"
+  >
+
+  </vue-recaptcha>
+</template>
+
+<script>
+import VueRecaptcha from 'vue-recaptcha';
+export default {
+  name: "DisplayRecaptcha",
+  components: { VueRecaptcha },
+  methods: {
+    successVerify( response ) {
+      console.log(response)
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
